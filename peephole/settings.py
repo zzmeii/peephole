22ebef474f2cb14647ebeb61dab0e1e2e0072326
Logvinovich Oleg <logvinovicholeg@gmail.com>
2023-08-20 03:18:31 +0700

    requirements.txt Postgres connection blank for base html

--- peephole/settings.py
+++ peephole/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'peephole.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'peepholeDB',
+        'USER': 'peepholeAdmin',
+        'PASSWORD': 'StrongPassword',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
