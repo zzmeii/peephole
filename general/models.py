e50705c7eb8e0fe3cb72052c27f7b04420d20fdb
VoznyukED <VoznyukED@gmail.com>
2023-08-20 22:46:58 +0300

    Add profile

--- general/models.py
+++ general/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Profile (models.Model):
+    user = models.OneToOneField(to=User, verbose_name='Пользователь', related_name='profile', on_delete=models.DO_NOTHING)
+    father_name = models.CharField(verbose_name='Отчество', max_length=64, default='')
